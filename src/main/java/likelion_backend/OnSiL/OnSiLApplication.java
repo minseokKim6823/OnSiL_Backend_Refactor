09ac3f45eb590e7b66f4cd2f726c6f693242b07b
minseokKim6823 <alstjr6823@naver.com>
2024-07-26 14:23:05 +0900

    initial commit

---
+++ src/main/java/likelion_backend/OnSiL/OnSiLApplication.java
@@ -0,0 +1,13 @@
+package likelion_backend.OnSiL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnSiLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnSiLApplication.class, args);
+	}
+
+}
---
+++ src/test/java/likelion_backend/OnSiL/OnSiLApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion_backend.OnSiL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnSiLApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

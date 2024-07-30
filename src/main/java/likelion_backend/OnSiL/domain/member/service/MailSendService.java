2007c19f1e2fb44f1e0524c29629cf49eef25507
minseokKim6823 <alstjr6823@naver.com>
2024-07-30 14:14:59 +0900

    fix:서비스용 이메일 만듦

--- src/main/java/likelion_backend/OnSiL/domain/member/service/MailSendService.java
+++ src/main/java/likelion_backend/OnSiL/domain/member/service/MailSendService.java
@@ -46,7 +46,7 @@ public class MailSendService {
     }
 
 
-    //mail을 어디서 보내는지, 어디로 보내는지 , 인증 번호를 html 형식으로 어떻게 보내는지 작성합니다.
+    //mail을 어디서 보내는지, 어디로 보내는지 , 인증 번호를 html 형식으로 어떻게 보내는지 작성
     public String joinEmail(String email) {
         makeRandomNumber();
         String setFrom = "likelionsmu@naver.com"; // email-config에 설정한 자신의 이메일 주소를 입력

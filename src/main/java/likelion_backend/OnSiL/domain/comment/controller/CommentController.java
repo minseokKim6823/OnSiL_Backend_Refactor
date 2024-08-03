6ba4e3527135652b6e816d23b67e5c9c96cdb980
Raymond Yoon <yoonjunsang@naver.com>
2024-08-03 20:00:28 +0900

    설명 및 이름추가

--- src/main/java/likelion_backend/OnSiL/domain/comment/controller/CommentController.java
+++ src/main/java/likelion_backend/OnSiL/domain/comment/controller/CommentController.java
@@ -1,5 +1,6 @@
 package likelion_backend.OnSiL.domain.comment.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import likelion_backend.OnSiL.domain.comment.dto.CommentRequest;
 import likelion_backend.OnSiL.domain.comment.entity.Comment;
 import likelion_backend.OnSiL.domain.comment.service.CommentService;
@@ -16,11 +17,13 @@ public class CommentController {
     private CommentService commentService;
 
     @PostMapping
+    @Operation(summary = "댓글 생성 //준상")
     public Comment createComment(@RequestBody CommentRequest commentRequest, @AuthenticationPrincipal UserDetails userDetails) {
         return commentService.createComment(commentRequest, userDetails.getUsername());
     }
 
     @DeleteMapping("/{commentId}")
+    @Operation(summary = "댓글 삭제 //준상")
     public String deleteComment(@PathVariable Long commentId, @AuthenticationPrincipal UserDetails userDetails) {
         commentService.deleteComment(commentId, userDetails.getUsername());
         return "Comment removed";
--- src/main/java/likelion_backend/OnSiL/domain/like/controller/LikeController.java
+++ src/main/java/likelion_backend/OnSiL/domain/like/controller/LikeController.java
@@ -1,5 +1,6 @@
 package likelion_backend.OnSiL.domain.like.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import likelion_backend.OnSiL.domain.like.service.LikeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,12 +15,14 @@ public class LikeController {
     private LikeService likeService;
 
     @PostMapping("/{postId}")
+    @Operation(summary = "좋아요 추가 //준상")
     public String createLike(@PathVariable int postId, @AuthenticationPrincipal UserDetails userDetails) {
         likeService.createLike(postId, userDetails.getUsername());
         return "Like added";
     }
 
     @DeleteMapping("/{likeId}")
+    @Operation(summary = "좋아요 삭제 //준상")
     public String deleteLike(@PathVariable Long likeId) {
         likeService.deleteLike(likeId);
         return "Like removed";

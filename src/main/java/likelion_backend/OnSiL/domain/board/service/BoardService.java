011f9ce5fb00ded13ea3aad601ad6166e24c77db
minseokKim6823 <alstjr6823@naver.com>
2024-08-05 01:07:11 +0900

    feat: 게시글 글쓴이 추가

--- src/main/java/likelion_backend/OnSiL/domain/board/service/BoardService.java
+++ src/main/java/likelion_backend/OnSiL/domain/board/service/BoardService.java
@@ -42,29 +42,6 @@ public class BoardService {
     private final MemberRepository memberRepository;
     private final S3FileUploadController s3FileUploadController;
 
-    /*@Transactional
-   public void save(BoardRequestDTO boardDTO) {
-       try {
-           Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-           if (authentication == null) {
-               throw new IllegalStateException("인증 정보가 없습니다.");
-           }
-           String currentUserEmail = authentication.getName();
-           log.info("현재 사용자의 이메일: {}", currentUserEmail);
-           Board board = new Board();
-           board.setWriter(currentUserEmail);
-           board.setTitle(boardDTO.getTitle());
-           board.setContent(boardDTO.getContent());
-           board.setImage(boardDTO.getImage());
-           board.setCategory(boardDTO.getCategory());
-           board.setRecommend(0);
-           boardRepository.save(board);
-           log.info("게시물 저장 성공: {}", board);
-       } catch (Exception e) {
-           log.error("게시물 저장 실패", e);
-           throw new RuntimeException("게시물 저장 중 오류가 발생", e);
-       }
-   } */
     @Transactional
     public void save(BoardRequestDTO boardDTO) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
